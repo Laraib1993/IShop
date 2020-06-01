979c6d0ff10584bae69067765258024152a9143f
laraib <s.laraibtirmizy@gmail.com>
2020-06-01 18:22:07 +0500

    Version 1

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDRs4fs_dZkIT6puFqn09X3OgYADAEDjCk",
+    authDomain: "ishop-ea2bb.firebaseapp.com",
+    databaseURL: "https://ishop-ea2bb.firebaseio.com",
+    projectId: "ishop-ea2bb",
+    storageBucket: "ishop-ea2bb.appspot.com",
+    messagingSenderId: "1096224959636"
+  }
+};
